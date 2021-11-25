e6b36d5779779d1e8cb4b7f85b166d0c28196b58
Kumar Reddy <z993415@thryv.com>
2021-11-25 11:40:36 -0600

    Completed module

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,7 +6,7 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
-    for path in source.rglog("*"):
+    for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
